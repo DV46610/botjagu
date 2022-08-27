4b7fd73c16eb6211f57b1b12df5c490d31313072
DV46610 <110512675+DV46610@users.noreply.github.com>
2022-08-27 09:50:20 +0530

    Update and rename demo.html to demo.java

--- demo.java
+++ demo.java
@@ -3,7 +3,7 @@ class Swap
 {
 public static void main(String[] args)
 {
-int x=10, y=20;// x and y are to swap
+int x=10, y=420;// x and y are to swap
 int temp;
 System.out.println("before swapping numbers: x = "+x +" and y = "+ y);
 /*swapping */
